c0160d957b9a517408ecb0109bd51e21e0e9c26a
Ramesh Raju Alluri <g801691@ad.syniverse.com>
2017-02-14 22:34:24 +0530

    added basic chart

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,4 @@
-var app=angular.module('app',[]);
-
+var app=angular.module('app',['chart.js']);
+(function (ChartJsProvider) {
+  ChartJsProvider.setOptions({ colors : [ '#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'] });
+}); 
--- public/js/ctrl.js
+++ public/js/ctrl.js
@@ -2,10 +2,46 @@ app.controller('appCtrl',AppCtrl);
 
 AppCtrl.$inject=['$scope','DailReport'];
 function AppCtrl($scope,DailReport){
+    //Chart Data
+     $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  $scope.data = [300, 500, 100];
     
-      $scope.columns={};
+    // Chart 
+    $scope.lccolors = ['#45b7cd', '#ff6384', '#ff8e72'];
+
+    $scope.lclabels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    $scope.lcdata = [
+      [65, -59, 80, 81, -56, 55, -40],
+      [28, 48, -40, 19, 86, 27, 90]
+    ];
+    $scope.datasetOverride = [
+      {
+        label: "Bar chart",
+        borderWidth: 1,
+        type: 'bar'
+      },
+      {
+        label: "Line chart",
+        borderWidth: 3,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        type: 'line'
+      }
+    ];
+    // Bar chart
+     $scope.blabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.bseries = ['Series A', 'Series B'];
+
+  $scope.bdata = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+    // Chart end
+    
+    
+    $scope.columns={};
     $scope.dailyData=DailReport.getData();
-      DailReport.getRestData();
+    DailReport.getRestData();
     $scope.btnGet=function(){
         console.log('GetData');
         DailReport.getRestData();
