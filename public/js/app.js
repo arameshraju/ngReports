c00766c53eead25499acd4d880ef346f20211f73
Ramesh Raju Alluri <g801691@ad.syniverse.com>
2017-03-22 18:26:40 +0530

    aded base view fo rside menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app=angular.module('app',['chart.js']);
+var app=angular.module('app',['ui.bootstrap', 'chart.js']);
 (function (ChartJsProvider) {
   ChartJsProvider.setOptions({ colors : [ '#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'] });
 }); 
